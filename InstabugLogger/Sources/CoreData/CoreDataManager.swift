ad3bc838508cf8c7f7286916db7d5ee7cfb94844
Ahmad Yasser <42544598+ahmdyasser@users.noreply.github.com>
2022-05-27 17:00:09 +0200

    refactor CoreDatManager

--- InstabugLogger/Sources/CoreData/CoreDataManager.swift
+++ InstabugLogger/Sources/CoreData/CoreDataManager.swift
@@ -25,15 +25,27 @@ class CoreDataManager {
         return container
     }()
     
+    
+    lazy var backgroundContext: NSManagedObjectContext = {
+       let context = persistentContainer.newBackgroundContext()
+       context.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+       return context
+     }()
+
+    
+     lazy var mainContext: NSManagedObjectContext = {
+       let context = persistentContainer.viewContext
+       context.automaticallyMergesChangesFromParent = true
+       return context
+     }()
+    
     // MARK: - Core Data Saving support
     
     func saveContext () {
         
-        let context = persistentContainer.viewContext
-        
-        if context.hasChanges {
+        if mainContext.hasChanges {
             do {
-                try context.save()
+                try mainContext.save()
             } catch {
                 fatalError("Unresolved error \(error)")
             }
@@ -52,11 +64,8 @@ extension CoreDataManager {
     ///     - index:  The index at which you delete data.
     ///     - deletedData:  The data before deleting.
     func deleteLogAt(index: Int, deletedData: [LoggerEntity]) {
-        
-        let _ = persistentContainer.performBackgroundTask { context in
-            context.delete(deletedData[index])
+            mainContext.delete(deletedData[index])
             self.saveContext()
-        }
         
     }
     
@@ -65,14 +74,13 @@ extension CoreDataManager {
     /// - Parameters:
     ///     - entity:  The entity name which you want to clear from
     func clearLog(for entity: String) {
-        let context = persistentContainer.viewContext
         let request = NSFetchRequest<LoggerEntity>(entityName: entityName)
         request.returnsObjectsAsFaults = false
         
         do {
-            let results = try context.fetch(request)
+            let results = try mainContext.fetch(request)
             for managedObject in results {
-                context.delete(managedObject)
+                mainContext.delete(managedObject)
             }
         } catch let error as NSError {
             print("Delete all data in \(entity) error : \(error) \(error.userInfo)")
@@ -86,11 +94,10 @@ extension CoreDataManager {
     /// - Parameters:
     ///     - logger:  A logger instance
     func saveLog(logger: LoggerModel) {
-        let context = persistentContainer.viewContext
         
         let fetchRequest = NSFetchRequest<LoggerEntity>(entityName: self.entityName)
-        let oldLogs = try! context.fetch(fetchRequest)
-        let newLogs = LoggerEntity(context: context)
+        let oldLogs = try! mainContext.fetch(fetchRequest)
+        let newLogs = LoggerEntity(context: mainContext)
         
         newLogs.message = logger.message
         newLogs.level = logger.level
@@ -98,9 +105,9 @@ extension CoreDataManager {
         if oldLogs.count >= 1000 {
             self.deleteLogAt(index: 0, deletedData: oldLogs)
         }
-        context.insert(newLogs)
+        mainContext.insert(newLogs)
         do {
-            try context.save()
+            try mainContext.save()
         } catch {
             fatalError("Can't save logs \(error)")
         }
@@ -110,11 +117,10 @@ extension CoreDataManager {
         
     }
     func fetchLogs(forEntityName entity: String, completion: @escaping ([LoggerEntity]) -> Void) {
-        let context = persistentContainer.viewContext
         var logs: [LoggerEntity] = []
         let fetchRequest = NSFetchRequest<LoggerEntity>(entityName: self.entityName)
         do {
-            logs = try context.fetch(fetchRequest)
+            logs = try mainContext.fetch(fetchRequest)
             completion(logs)
         } catch {
             fatalError("Can't fetch logs \(error)")
